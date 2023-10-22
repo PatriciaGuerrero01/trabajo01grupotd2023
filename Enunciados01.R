e81cc3bca6f43ec640571fd25760e900081750f5
qNicop <135727284+qNicop@users.noreply.github.com>
2023-10-22 14:53:27 +0200

    Añado enunciados Nico

--- Enunciados01.R
+++ Enunciados01.R
@@ -42,3 +42,38 @@ X
 #de venta aumenta a 1,50€ el litro. Por el contrario, si durante el año no llueve, la producción de aceite disminuye 
 #a por cada 3 kilos de aceitunas se hace un litro de aceite y el precio de venta de un litro de aceite aumenta hasta 2,50€.
 #Por otra parte si se alquila se hace por 150000€ y la empresa produce y gana el 10% de la producción anual.
+
+## PROBLEMA 5
+Aplicar los criterios de decisión bajo incertidumbre a los problemas cuya matriz de valores viene dada e la tabla siguiente.  
+Considerar beneficios (favorable) y costos (desfavorable)  
+
+```{r echo=FALSE}
+source("teoriadecision_funciones_incertidumbre.R")
+m1A <- crea.tablaX(c(7,5,1,
+                     -3,1,3,
+                     5,-1,3,
+                     5,4,-10),
+                   numalternativas = 4, numestados = 3)
+m1A
+```
+
+## PROBLEMA 6
+
+**Elección de un Transporte para el Trabajo**
+
+Nico está considerando sus opciones de transporte para ir al trabajo. Tiene tres alternativas:
+
+1. **Conducir su propio automóvil:** Nico puede conducir su automóvil al trabajo. El costo mensual de gasolina y estacionamiento es de 200 euros. Nico tiene un colega que trabaja cerca de su trabajo, con el que puede compartir el gasto de la gasolina a medias si coinciden en horario
+
+2. **Tomar el transporte público:** Nico puede optar por usar el transporte público para ir al trabajo. El costo mensual del pase de transporte público es de 80. Sin embargo, si su colega opta por ir con Nico en transporte público pueden sacar un bono con un 15% de descuento para ambos.
+
+3. **Blablacar:** Nico puede por irse al trabajo en blablacar costandole el transporte 160 euros mensuales,si su colega se apunta con Nico al anunciante de blablacar el transporte le costaria un 30% menos.
+
+Nico tiene dos estados de la naturaleza para considerar:
+
+**Estado de la Naturaleza 1: Disponibilidad del Colega:** En este estado, su colega está disponible para compartir el viaje.
+
+**Estado de la Naturaleza 2: Indisponibilidad del Colega:** En este estado, su colega no puede compartir el viaje.
+
+¿Cuál es la opción más conveniente para Nico en términos de costo total durante el primer año? Representa las ecuaciones de alternativas para cada opción de transporte y estado de la naturaleza
+
